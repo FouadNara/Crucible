fa9ca22c9fd93fdc86b9197fa3188d3f6d8a5734
FouadNara <fznara@gmail.com>
2025-09-09 11:47:18 +0300

    added some notes

--- Elements_of_Programming.cpp
+++ Elements_of_Programming.cpp
@@ -10,3 +10,21 @@
 #include <cctype>
 #include <cstdlib>
 
+/*
+1. Clarify the problem: if a problem is not clear, ask questions to the interviewer.
+2. Think out loud: explain your thought process to the interviewer.
+3. Write down the steps: write down the steps you will take to solve the problem.
+4. Implement the solution: implement the solution.
+5. Test the solution: test the solution.
+6. Optimize the solution: optimize the solution.
+7. Write the code: write the code.
+8. Test the code: test the code.
+9. Debug the code: debug the code.
+10. Submit the solution: submit the solution.
+
+feel free to ask the interviewer what time complexity is expected and what is the expected space complexity.
+even if he is evasive, you may get a hint from his answer.
+
+work on concrete examples.
+*/
+
